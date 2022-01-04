8192de06f430dfdd4b18697f784290fc88988747
elcaskerito <idimhtt4t@gmail.com>
2022-01-04 19:59:02 +0100

    correction de la boucle while et ajout de for

--- idi.java
+++ idi.java
@@ -20,17 +20,20 @@ class idi{
     public static void main(String[] args){
 
         int a,b,c=0;
-        String text= new String();
+        // String text= new String();
+        char text=' ';
         System.out.println("Un pg qui permet de testez si la somme des deux nombres est supérieur ou inférieur a 100\n");
-        while (text!="Q")
+        while (text!='Q')
         {
      
         System.out.println("entrer le nombre A : ");
         a=enter.nextInt();
-        enter.nextLine();
+        // enter.nextLine();
        
         System.out.println("enter le nombre B :");
         b=enter.nextInt();
+        // enter.nextLine();
+        int i=10;
         c=a+b;
         if (c>100){
             System.out.println(" Super, la somme de A+B est > a 100  :"+(a+b));
@@ -42,10 +45,18 @@ class idi{
         }
         else{
             System.out.println("la somme est inférieur a 100, Somme= :"+(a+b));
-            enter.nextLine();
+          
+            
         }
+        // enter.nextLine();
+        for(int x=0; x<i;x++){
+            System.out.println("j'increment le resultat "+c++);
+        }
+        enter.nextLine();
+
         System.out.println("Taper Q pour quitter et O pour continuer   :");
-        text=enter.nextLine();
+        text=enter.nextLine().charAt(0);
+        
     }
      }
 
