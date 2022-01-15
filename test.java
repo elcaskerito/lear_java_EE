e7ac969bac0e47069bd90a35d5aa9419433b0892
elcaskerito <idimhtt4t@gmail.com>
2022-01-15 11:01:21 +0100

    ajout et progression

--- test.java
+++ test.java
@@ -2,8 +2,8 @@ package lear_java_EE;
 
 public class test {
 
-    public static void main(String args){
-    basic_construct idi=new basic_construct("idris", "mahamat");
+    public static void main(String[] args){
+    // basic_construct idi=new basic_construct("idris", "mahamat");
     System.out.println("iii");
 
     }
